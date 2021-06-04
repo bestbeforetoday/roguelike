3553ddc71adeb77e26d24c08556afd4fe8d8e91a
Mark S. Lewis <mark.s.lewis@outlook.com>
2021-06-04 11:11:11 +0000

    WIP: Convert to TypeScript

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "roots": [
+        "<rootDir>/src"
+    ],
+    "preset": "ts-jest",
+    "testEnvironment": "node",
+    "collectCoverage": true,
+    "collectCoverageFrom": [
+        "**/*.[jt]s?(x)",
+        "!**/*.d.ts",
+    ],
+    "coverageProvider": "v8",
+    "testMatch": [
+        "**/?(*.)+(spec|test).[jt]s?(x)"
+    ],
+}
